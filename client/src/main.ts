21528c9b69e8dbdd7ea5dfd00295db48b3b9627b
David Vargas <dav793@gmail.com>
2024-09-29 20:06:44 -0600

    Agregar escena basica

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,2 +1,51 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-alert("Hola mundo");
+// create renderer
+const container = document.getElementById('canvas-container');
+const renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+container.appendChild( renderer.domElement );
+
+// create scene + scene objects
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    45, window.innerWidth / window.innerHeight, 0.1, 100
+);
+camera.position.set( 0, 2, 4 );
+camera.lookAt( 0, 0, 0 );
+scene.add( camera );
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 3 );
+directionalLight.position.set( 2, 10, 1 );
+directionalLight.lookAt( 0, 0, 0 );
+scene.add( directionalLight );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshBasicMaterial({ color: 0x4368e0 });
+const material = new THREE.MeshPhongMaterial({ color: 0x4368e0 });
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+// render frames
+function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+}
+animate();
+
+// listen screen dimension changes
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+});
